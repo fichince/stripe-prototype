7021d786ec5dd108c6f0e43c9ee969fa26cd9d6c
fichince <acchoi@gmail.com>
2022-09-08 09:09:42 -0400

    Setting up embedded form

--- src/routes/elements/+page.svelte
+++ src/routes/elements/+page.svelte
@@ -1,7 +1,48 @@
-<script lang="ts">
-  import type { PageData } from './$types';
+<svelte:head>
+  <script src="https://js.stripe.com/v3/"></script>
+</svelte:head>
 
-  export let data : PageData;
+<script context="module" lang="ts">
+  declare var Stripe : any;
 </script>
 
-{data.clientSecret}
+<script lang="ts">
+  import type { PageData } from './$types';
+  import { onMount } from 'svelte';
+  import { env } from '$env/dynamic/public';
+
+  export let data : PageData;
+
+  let elements : any;
+
+  onMount(() => {
+    const { clientSecret } = data;
+    const stripe = Stripe(env.PUBLIC_STRIPE_KEY);
+
+    const appearance = { theme: 'stripe' };
+    elements = stripe.elements({ appearance, clientSecret });
+
+    const paymentElement = elements.create('payment');
+    paymentElement.mount('#payment-element');
+  });
+</script>
+
+<div class="container">
+  This is the embedded payment form
+
+  <div class="form-container">
+    <div id="payment-element"></div>
+  </div>
+</div>
+
+<style>
+  .container {
+    padding: 50px;
+  }
+
+  .form-container {
+    border: 1px solid black;
+    padding: 20px;
+    margin: 10px;
+  }
+</style>
